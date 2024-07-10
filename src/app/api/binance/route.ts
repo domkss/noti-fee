4dc05f34d4b01adf00600a81487590f030545e47
Dominik Kiss <kissdominik7@gmail.com>
2024-07-10 18:56:16 +0200

    Change the 5 min revalidation of binance fees rout to a full dynamic rout

--- src/app/api/binance/route.ts
+++ src/app/api/binance/route.ts
@@ -20,5 +20,4 @@ export async function GET(req: NextRequest) {
   return NextResponse.json(response);
 }
 
-export const dynamic = "auto";
-export const revalidate = 300;
+export const dynamic = "force-dynamic";
